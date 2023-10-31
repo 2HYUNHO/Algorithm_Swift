3aab0a33af4ee90c89c63699a88ebe1b4cb603eb
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-10-31 11:22:54 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+func solution(_ num_list:[Int]) -> Int {
+  
+ return Int(num_list.filter{$0 % 2 != 0}.map{ (String($0)) }.joined())! + Int(num_list.filter{$0 % 2 == 0}.map{ (String($0)) }.joined())!
+}
