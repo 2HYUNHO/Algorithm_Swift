24d47fe70b0b2144ddabedd05b1c7c33d39da2ad
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-10-31 19:04:51 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
@@ -3,11 +3,9 @@ import Foundation
 func solution(_ numbers:[Int], _ n:Int) -> Int {
     var sum: Int = 0
     
-    for i in numbers {
+    // sum이 n보다 작거나 같을 때 까지만 더함
+    for i in numbers where sum <= n {
         sum += i
-        if sum > n {
-            break
-        }
     }
     return sum
 }
