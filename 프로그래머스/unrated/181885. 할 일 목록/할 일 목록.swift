0e601a3af94fddbd363fd82f67668c6e0ed4f5f5
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-10-31 14:27:59 +0900

    [level unrated] Title: 할 일 목록, Time: 1.99 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ todo_list:[String], _ finished:[Bool]) -> [String] {
+    var result : [String] = []
+    for (i, v) in finished.enumerated() {
+        for (j, a) in todo_list.enumerated() {
+            if i == j && v == false {
+                result.append(a)
+            } 
+        }
+        }
+        return result
+}
