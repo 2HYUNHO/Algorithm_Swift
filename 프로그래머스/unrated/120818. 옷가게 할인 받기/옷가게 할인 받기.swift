b16aa99b396b57c2cca9366a34f38d1a41048bc2
Hyunho <102159946+2HYUNHO@users.noreply.github.com>
2023-10-31 11:21:32 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+func solution(_ price:Int) -> Int {
+    return price >= 500000 ? Int(Double(price) * Double(0.8)) : price >= 300000 ? Int(Double(price) * Double(0.9)) : price >= 100000 ? Int(Double(price) * Double(0.95)) : price
+}
+
